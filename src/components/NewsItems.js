cc6c8a0a8b1c3753c03a9fca14e220d3a44f44b3
Ashish Kumar <ns730106@gmail.com>
2025-03-31 08:10:10 +0530

    Added Author name article published date and publisher name

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 
 export class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
-        <div className="card" >
+        <div className="card">
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <span class="badge text-bg-secondary">Published by {source}</span>
+
+            <p className="card-text">
+              <small className="text-muted">
+                Author {author ? author : "Unknown"} published on
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               className="btn btn-sm btn-dark"
